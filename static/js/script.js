515ad78f8abdeffd2f8013826f665beafc745a34
Matthew Cleghorn <mr.m.cleghorn@gmail.com>
2024-07-22 10:09:38 +0000

    Bind data from db to accordion

--- static/js/script.js
+++ static/js/script.js
@@ -4,5 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
  
     let collapsible = document.querySelectorAll('.collapsible');
     M.Collapsible.init(collapsible);
+
+    let tooltip = document.querySelectorAll('.tooltipped');
+    M.Tooltip.init(tooltip);
     
   });
